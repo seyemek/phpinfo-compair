2a363d0c3a19b65014555f1e22283bc837502e58
seyemek <103988602+seyemek@users.noreply.github.com>
2022-11-09 12:52:14 +0300

    Update info.php

--- info.php
+++ info.php
@@ -5,12 +5,9 @@
 	$server[] = ['name' => '127.0.0.2 - PHP 8',		'url'=> 'http://127.0.0.2/symk/info.php'];
 	//$server[] = ['name' => '127.0.0.3 - PHP 5',		'url'=> 'https://127.0.0.3/symk/info.php'];
 	/**/
-
 	include('inc/template.php');
 	include('inc/class.php');
-
 	$info = new compair();
-	
 	$info->servers				= $server;
 	$info->is_only_err			= 1; //0 all, 1 only err, 2 only no-err
 	$info->hide_heads			= [];
@@ -18,12 +15,8 @@
 	$info->compair				= ['Loaded Modules' => ' ', 'disable_functions' => ',', 'Protocols' => ', ', 'Hashing Engines' => ' ', 'Interfaces' => ', ', 'Classes' => ', '];
 	$info->compair_hide_existing = true;
 	$info->nohide_item			= ['Apache Version', 'Hostname:Port', 'Host', 'PHP Version'];
-
 	$info->tpl = new Template("style/tpl/");
 	$info->info();
-	//print_r($info->phpinfos);
-	//exit;
 	$info->tpl->set_filenames(array('body' => 'comparison.html'));
 	$info->tpl->pparse('body');
-	
 ?>
